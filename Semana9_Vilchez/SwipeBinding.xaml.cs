dba33aa2c65272f08fe1d61d48451b381ead4422
hdelacruz-tecsup <hugo.delacruz@tecsup.edu.pe>
2020-05-26 22:23:07 -0500

    v1

---
+++ Semana9_Vilchez/SwipeBinding.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana9_Vilchez
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SwipeBinding : ContentPage
+    {
+        public SwipeBinding()
+        {
+            InitializeComponent();
+            BindingContext = new SwipeCommandPageViewModel();
+        }
+    }
+}
---
+++ Semana9_Vilchez/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Semana9_Vilchez.Models;
+
+namespace Semana9_Vilchez.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
